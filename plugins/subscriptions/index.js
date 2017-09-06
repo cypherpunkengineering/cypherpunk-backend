170f008cdcb5f8a41faaf659e24fd1925e35c3a1
Edward Kim <ed@slickage.com>
2017-09-05 22:57:30 -1000

    Start subscription plugin
    Currently it only supports a function to calculate a renewal date.

---
+++ plugins/subscriptions/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  calculateRenewal: (plan) => {
+    let subscriptionStart = new Date();
+    let subscriptionRenewal = new Date(+subscriptionStart);
+
+    if (plan === 'trial') {
+      subscriptionRenewal.setDate(subscriptionStart.getDate() + 1);
+    }
+    else if (plan.startsWith('monthly')) {
+      subscriptionRenewal.setDate(subscriptionStart.getDate() + 30);
+    }
+    else if (plan.startsWith('semiannually')) {
+      subscriptionRenewal.setDate(subscriptionStart.getDate() + 180);
+    }
+    else if (plan.startsWith('annually')) {
+      subscriptionRenewal.setDate(subscriptionStart.getDate() + 365);
+    }
+    else { return 0; }
+
+    return subscriptionRenewal;
+  }
+};
