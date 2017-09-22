26e84345a258a3702028557dc1b158ece94e4d95
Ed <taesup63@gmail.com>
2017-09-22 13:42:26 -1000

    Advanced users analytics in admin ccp

--- routes/admin/users/counts/get.js
+++ routes/admin/users/counts/get.js
@@ -9,35 +9,124 @@ module.exports = {
   },
   handler: (request, reply) => {
     let promises = [
-      request.db('users').count('type').where({ type: 'trial' }).then(scalar),
-      request.db('users').count('type').where({ type: 'pending' }).then(scalar),
-      request.db('users').count('type').where({ type: 'invitation' }).then(scalar),
-      request.db('users').count('type').where({ type: 'free' }).then(scalar),
-      request.db('users').count('type').where({ type: 'premium' }).then(scalar),
+      trialUsers(request),
+      confirmedTrials(request),
+      unconfirmedTrials(request),
+      premiumUsers(request),
+      activeUsers(request),
+      activeCancelledUsers(request),
+      inactiveUsers(request),
       request.db.select('count').from('user_counters').where({ type: 'registered' }).then(scalar),
       request.db.select('count').from('user_counters').where({ type: 'confirmed' }).then(scalar),
     ];
 
-    function scalar(data) {
-      if (data.length) { return parseInt(data[0].count); }
-      else { return 0; }
-    }
-
     let promise = Promise.all(promises)
     .then((counts) => {
-      console.log(counts);
-
       return {
-        trial: counts[0],
-        pending: counts[1],
-        invitation: counts[2],
-        free: counts[3],
-        premium: counts[4],
-        registered: counts[5],
-        confirmed: counts[6]
+        trial: {
+          total: counts[0],
+          confirmed: counts[1],
+          unconfirmed: counts[2]
+        },
+        premium: {
+          total: counts[3],
+          active: counts[4],
+          cancelled: counts[5],
+          inactive: counts[6],
+        },
+        total: {
+          registered: counts[7],
+          confirmed: counts[8]
+        }
       };
     })
     .catch((e) => { return Boom.badImplementation(e); });
     return reply(promise);
   }
 };
+
+
+function scalar(data) {
+  if (data.length) { return parseInt(data[0].count); }
+  else { return 0; }
+}
+
+function trialUsers(request) {
+  return request.db('users')
+  .count('type')
+  .whereIn('type', ['trial', 'pending', 'invitation', 'free'])
+  .then(scalar);
+}
+
+function confirmedTrials(request) {
+  return request.db('users')
+  .count('type')
+  .whereIn('type', ['trial', 'pending', 'invitation', 'free'])
+  .andWhere('confirmed', true)
+  .then(scalar);
+}
+
+function unconfirmedTrials(request) {
+  return request.db('users')
+  .count('type')
+  .whereIn('type', ['trial', 'pending', 'invitation', 'free'])
+  .andWhere('confirmed', false)
+  .then(scalar);
+}
+
+function premiumUsers(request) {
+  return request.db('users')
+  .count('type')
+  .where('type', 'premium')
+  .then(scalar);
+}
+
+function activeUsers(request) {
+  return request.db('users')
+  .join('subscriptions', 'users.id', 'subscriptions.user_id')
+  .where({ 'users.type': 'premium' })
+  .andWhere('subscriptions.current', '=', true)
+  .andWhere('subscriptions.current_period_end_timestamp', '>', request.db.fn.now())
+  .whereNull('subscriptions.cancellation_timestamp')
+  .whereNull('subscriptions.renewal_timestamp')
+  .count('users.type')
+  .then(scalar);
+}
+
+function activeCancelledUsers(request) {
+  return request.db('users')
+  .join('subscriptions', 'users.id', 'subscriptions.user_id')
+  .where({ 'users.type': 'premium' })
+  .andWhere('subscriptions.current', '=', true)
+  .andWhere('subscriptions.current_period_end_timestamp', '>', request.db.fn.now())
+  .andWhere(function() {
+    this.whereNotNull('subscriptions.cancellation_timestamp')
+    .orWhereNotNull('subscriptions.renewal_timestamp');
+  })
+  .count('users.type')
+  .then(scalar);
+}
+
+function inactiveUsers(request) {
+  let promises = [];
+
+  promises.push(request.db('users')
+    .join('subscriptions', 'users.id', 'subscriptions.user_id')
+    .where({ 'users.type': 'premium' })
+    .where('subscriptions.current', '=', true)
+    .andWhere('subscriptions.current_period_end_timestamp', '<', request.db.fn.now())
+    .count('users.type')
+    .then(scalar)
+  );
+
+  promises.push(request.db('users')
+    .leftJoin('subscriptions', 'users.id', 'subscriptions.user_id')
+    .where({ 'users.type': 'premium' })
+    .whereNull('subscriptions.id')
+    .count('users.type')
+    .then(scalar)
+  );
+
+  return Promise.all(promises)
+  .then(data => { return data[0] + data[1]; });
+}
--- web/src/app/shared/cards/users/users-card.component.ts
+++ web/src/app/shared/cards/users/users-card.component.ts
@@ -8,24 +8,10 @@ import { BackendService } from '../../../services/backend.service';
 })
 export class UsersCardComponent {
   loading = true;
-  trialUsers: number;
-  pendingUsers: number;
-  invitationUsers: number;
-  freeUsers: number;
-  premiumUsers: number;
-  registeredUsers: number;
-  confirmedUsers: number;
+  users: any;
 
   constructor(private backend: BackendService) {
     this.backend.userCounts()
-    .then((counts) => {
-      this.trialUsers = counts.trial;
-      this.pendingUsers = counts.pending;
-      this.invitationUsers = counts.invitation;
-      this.freeUsers = counts.free;
-      this.premiumUsers = counts.premium;
-      this.registeredUsers = counts.registered;
-      this.confirmedUsers = counts.confirmed;
-    });
+    .then((data) => { this.users = data; });
   }
 }
