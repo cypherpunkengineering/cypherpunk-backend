5e5cd51d24e9a9fda5ef99db97e62527a315eb58
Kim Nilsson <kim@cypherpunk.com>
2017-09-23 09:32:40 +0900

    Normalize NODE_ENV values

--- configs/general.js
+++ configs/general.js
@@ -1,5 +1,26 @@
+var env = (process.env.NODE_ENV || 'DEV').toUpperCase();
+switch (env) {
+  case 'DEV':
+  case 'PROD':
+  case 'STAGING':
+    break;
+  case 'DEVELOPMENT':
+    env = 'DEV';
+    break;
+  case 'PRODUCTION':
+    env = 'PROD';
+    break;
+  case 'STAGING':
+    env = 'STAGING';
+    break;
+  default:
+    console.warn("Unrecognized NODE_ENV, defaulting to 'DEV'");
+    env = 'DEV';
+    break;
+}
+
 module.exports = {
-  env: process.env.NODE_ENV || 'DEV', // ['DEV', 'PROD', 'STAGING']
+  env, // ['DEV', 'PROD', 'STAGING']
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 9000
 };
