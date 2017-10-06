7562dc31ea302b1808856a396b298f6c40b61440
Ed <taesup63@gmail.com>
2017-10-05 22:31:02 -1000

    Add token requirement to email unsubscribe route

--- routes/v1/emails/unsubscribe.js
+++ routes/v1/emails/unsubscribe.js
@@ -1,5 +1,8 @@
 const Joi = require('joi');
 const Boom = require('boom');
+const crypto = require('crypto');
+const algo = 'aes-256-ctr';
+const salt = 'jsucksballsformakingmedothisshit';
 
 module.exports = {
   method: 'GET',
@@ -8,21 +11,27 @@ module.exports = {
     auth: false,
     validate: {
       query: {
-        email: Joi.string().required()
+        email: Joi.string().required(),
+        token: Joi.string().required()
       }
     }
   },
   handler: (request, reply) => {
     let email = request.query.email;
+    let token = request.query.token;
 
+    // check token validity
+    let encrypted = encrypt(email);
+    if (token !== encrypted) { return reply(Boom.badRequest('Invalid Token')); }
+
+    // insert email into db
     let promise = request.db('unsubscribe_list').insert({ email: email })
     .then(() => { return 'Unsubscribed!'; })
     .catch((e) => {
-      console.log(e);
-
       // handle unique constraint error
       if (e.code === '23505') { return 'Unsubscribed!'; }
 
+      console.log(e);
       if (e.isBoom) { return e; }
       else { return Boom.badImplementation(); }
     });
@@ -30,3 +39,12 @@ module.exports = {
     return reply(promise);
   }
 };
+
+// ed@cypherpunk.com === 3ae7b7228cc9e3547579dd5f8bfb1c80b8
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algo, salt);
+  var crypted = cipher.update(text,'utf8','hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
